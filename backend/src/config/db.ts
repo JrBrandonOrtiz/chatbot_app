49d65decdaec3c3cb727ebdd6c5025ab8488c94a
brandonprogram4 <brandonprogram4@gmail.com>
2025-05-27 17:19:57 -0500

    port

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,8 +5,11 @@ dotenv.config();
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: Number(process.env.DB_PORT || 29524),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
